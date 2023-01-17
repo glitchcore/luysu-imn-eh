6facecb5c6a4e5d16707b080d72ada238aa61674
aanper <mail@s3f.ru>
2023-01-17 17:15:02 +0400

    move normalize to geometry

--- pi/boot/bydefault/geometry.py
+++ pi/boot/bydefault/geometry.py
@@ -1,4 +1,6 @@
 import numpy as np
+from math import *
+from calib_gallery import *
 
 def get_transformation_matrix_dlt(normalized_rect, plane_rect):
     X = np.array([point[0] for point in normalized_rect])
@@ -20,13 +22,35 @@ def denormalize_coordinates(normalized_coord, trans_matrix):
 
 normalized_rect = [(0, 0), (1, 0), (1, 1), (0, 1)]
 # plane_rect = [(3, 8), (10, 5), (14, 20), (2, 24)]
-plane_rect = [(0.1, 0.1), (10, 0.2), (10, 20), (0.1, 20)]
+
 trans_matrix = get_transformation_matrix_dlt(normalized_rect, plane_rect)
 
 def normalize_test():
     for a in normalized_rect:
         print(denormalize_coordinates(a, trans_matrix))
     print(denormalize_coordinates((0.5, 0.5), trans_matrix))
+
+def mach2cart(mach, calib):
+    p, q = mach
+
+    return (
+        (p**2 - q**2 + calib["W"]**2)/(2 * calib["W"]),
+        sqrt(p**2 - (p**2 - q**2 + calib["W"]**2)**2/(4 * calib["W"]**2))
+    )
+
+def cart2mach(cart, calib):
+    x, y = cart
+
+    return (
+        sqrt(x**2 + y**2),
+        sqrt((calib["W"] - x)**2 + y**2)
+    )
+
+def mach2grbl(mach, calib):
+    return(
+        (mach[0] - calib["init_a"]) * calib["R"],
+        (mach[1] - calib["init_b"]) * calib["R"],
+    )
 
 if __name__ == "__main__":
     normalize_test()
