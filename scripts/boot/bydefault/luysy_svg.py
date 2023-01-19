6dab04312aaa4d86223f4ea8b3928a7357bca8a9
aanper <mail@s3f.ru>
2023-01-19 12:08:38 +0400

    get all svgs

--- scripts/boot/bydefault/luysy_svg.py
+++ scripts/boot/bydefault/luysy_svg.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 import svg.path
+import glob
 
 def to_point(c, size):
     return (c.real / size[0], c.imag / size[1])
@@ -9,7 +10,7 @@ def parse_svg(filename):
     tree = ET.parse(filename)
     root = tree.getroot()
 
-    size = (float(root.attrib['width']), float(root.attrib['height']))
+    size = (float(root.attrib['width'].replace("m", "")), float(root.attrib['height'].replace("m", "")))
 
     # get the first path tag
     path_tag = root.find('.//{http://www.w3.org/2000/svg}path')
@@ -25,4 +26,21 @@ def parse_svg(filename):
 
 # 'web/svg/chr-savushkina-hackspace.svg'
 # web/svg/chr-test.svg
-print(parse_svg("web/svg/chr-savushkina-hackspace.svg"))
+# print(parse_svg("web/svg/chr-savushkina-hackspace.svg"))
+
+def get_files():
+    folder_path = 'svg'
+    svg_files = glob.glob(folder_path + '/*.svg')
+
+    data = []
+
+    for file in svg_files:
+        file_name = file.split("/")[-1]
+        parts = file_name.split("-", 1)
+        # print(parts)
+        data.append((parts[0], parse_svg(folder_path + '/' + file_name)))
+
+    return data
+
+if __name__ == '__main__':
+    print(get_files()[0])
